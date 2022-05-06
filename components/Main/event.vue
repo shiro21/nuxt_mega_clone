df9c6d754acf5db7571ddb57907ec601132a5259
노준혁 <junhyeok403@naver.com>
2022-05-06 18:55:55 +0900

    Fix : section scss 변경

--- components/Main/event.vue
+++ components/Main/event.vue
@@ -1,29 +1,31 @@
 <template>
-  <section>
-    <div class="container_box">
-      <div class="row_box">
-        <div class="event clearfix">
-          <h2>새로운 이벤트</h2>
-          <div class="event_left">
-            <div class="event_left_top">
-              <img :src="items[0].image" :srcset="items[0].srcset + ' 2x'" :alt="items[0].alt">
+  <div>
+    <section id="event">
+      <div class="container_box">
+        <div class="row_box">
+          <div class="event clearfix">
+            <h2>새로운 이벤트</h2>
+            <div class="event_left">
+              <div class="event_left_top">
+                <img :src="items[0].image" :srcset="items[0].srcset + ' 2x'" :alt="items[0].alt">
+              </div>
+              <div class="event_left_bottom_left">
+                <img :src="items[1].image" :srcset="items[1].srcset + ' 2x'" :alt="items[1].alt">
+              </div>
+              <div class="event_left_bottom_right">
+                <img :src="items[2].image" :srcset="items[2].srcset + ' 2x'" :alt="items[2].alt">
+              </div>
             </div>
-            <div class="event_left_bottom_left">
-              <img :src="items[1].image" :srcset="items[1].srcset + ' 2x'" :alt="items[1].alt">
-            </div>
-            <div class="event_left_bottom_right">
-              <img :src="items[2].image" :srcset="items[2].srcset + ' 2x'" :alt="items[2].alt">
-            </div>
-          </div>
-          <div class="event_right">
-            <div class="event_right_main">
-              <img :src="items[3].image" :srcset="items[3].srcset + ' 2x'" :alt="items[3].alt">
+            <div class="event_right">
+              <div class="event_right_main">
+                <img :src="items[3].image" :srcset="items[3].srcset + ' 2x'" :alt="items[3].alt">
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
